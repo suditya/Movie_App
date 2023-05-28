9bcba5210874f9c6d360ed9e80b4c11e8e91f5e2
suditya <guptasuditya@gmail.com>
2023-05-28 20:01:49 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
-// import ReactDOM from 'react-dom';
+import {useEffect} from "react";
+//21bb3c33
+const API_URL  ="http://www.omdbapi.com/?i=tt3896198&apikey=21bb3c33";
 const App = () =>
 {
-    return (
-        <h1>App</h1>
-    );
+    const searchMovies = async (title)=>
+    {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data)
+    }
+
+    useEffect(()=>
+    {
+
+    },[]);
 }
 
 export default App;
