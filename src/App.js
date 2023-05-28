a4cbed89b99119eedc479a5bc7707d7458b40897
suditya <guptasuditya@gmail.com>
2023-05-28 23:25:08 +0530

    added fav icon

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=21bb3c33";
 
-const movie1 =
-{
-    "Title": "The Avengers",
-    "Year": "2012",
-    "imdbID": "tt0848228",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
-}
+// const movie1 =
+// {
+//     "Title": "The Avengers",
+//     "Year": "2012",
+//     "imdbID": "tt0848228",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+// }
 
 const App = () => {
 
